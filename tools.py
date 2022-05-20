7dfd9bfc6a91deab563a3cbfa0dc2dd2f69e02c0
mikeymastro <magic9669@hotmail.com>
2022-05-20 10:18:26 -0400

    Updated tools.py for user/pw

--- tools.py
+++ tools.py
@@ -3,7 +3,7 @@
 import os
 
 
-def nornir_set_creds(norn, username="michael", password=None):
+def nornir_set_creds(norn, username, password):
     """
     Handler so credentials are not stored in cleartext.
     Thank you Kirk!
